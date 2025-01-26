57532d3997889b1ab03b8515cdec7ff5b02dd28d
ALMA_DEV914 <aungonalna58@gamil.com>
2025-01-25 20:21:24 -0800

    add services component

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React from "react";
+import services from "../assets/images/logo.jpg";
+import sevice1 from "../assets/images/scissor.png";
+import sevice2 from "../assets/images/shaver.png"
+import sevice3 from "../assets/images/mens.png"
+import service4 from "../assets/images/womens.png"
 
 const Services = () => {
   return (
-    <div>
-      Services
-    </div>
-  )
-}
+    <section
+      id="services"
+      className="w-full flex flex-col md:px-20 px-10 md:py-20 py-10 justify-center gap-16 items-center gap-16 bg-gray-900"
+    >
+      <div className="flex flex-col justify-center items-center gap-4">
+        <h1
+          data-aos="zoom-in"
+          className="text-6xl text-white font-bold text-center"
+        >
+          Barber Services
+        </h1>
+        <p data-aos="zoom-in" className="text-xl text-slate-200 text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, velit suscipit fuga dignissimos, id pariatur ratione sint cupiditate sed consectetur ducimus rerum nostrum incidunt in, facere odit ad aperiam animi!</p>
+      </div>
+      <div className="w-[90%] grid grid-cols-1 md:grid-cols-2 justify-center items-center gap-6">
+        <div id="image-box">
+          <img src={services} alt="" data-aos='zoom-in' data-aos-delay='200'/>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 justify-center items-center gap-8">
+        <div data-aos='zoom-out' data-aos-delay='200' className="bg-white px-8 py-12 flex-col justify-center items-center gap-4 rounde-xl">
+        <img src={sevice1} alt="" data-aos='zoom-in' data-aos-delay='200' className="sie-16 transform hover:scale:110 transition-transform duration-300 cursor-pointer"/>
+        <h1 className="text-black text-[25px] font-bold">Hair Care</h1>
+        </div>
+        
+        <div data-aos='zoom-out' data-aos-delay='200' className="bg-white px-8 py-12 flex-col justify-center items-center gap-4 rounded-xl">
+        <img src={sevice2} alt="" data-aos='zoom-in' data-aos-delay='200' className="size-16 transform hover:scale-110 transition-transform duration-300 cursor-pointer"/>
+        <h1 className="text-black text-[25px] font-bold">Hair Care</h1>
+        </div>
+        
+        <div data-aos='zoom-out' data-aos-delay='200' className="bg-white px-8 py-12 flex-col justify-center items-center gap-4 rounded-xl">
+        <img src={sevice3} alt="" data-aos='zoom-in' data-aos-delay='200' className="size-16 transform hover:scale-110 transition-transform duration-300 cursor-pointer"/>
+        <h1 className="text-black text-[25px] font-bold">Hair Care</h1>
+        </div>
+        
+        <div data-aos='zoom-out' data-aos-delay='200' className="bg-white px-8 py-12 flex-col justify-center items-center gap-4 rounded-xl">
+        <img src={service4} alt="" data-aos='zoom-in' data-aos-delay='200' className="size-16 transform hover:scale-110 transition-transform duration-300 cursor-pointer"/>
+        <h1 className="text-black text-[25px] font-bold">Hair Care</h1>
+        </div>
+      </div>
+      </div>
+    </section>
+  );
+};
 
-export default Services
+export default Services;
